e480322bfb2fdcd4927ac906ba1c7d8cc4851bb8
MalouTheCriss <saloisjacob74@gmail.com>
2024-02-28 13:29:57 -0500

    V0

---
+++ FileVideException.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * Classe d'exception utilisee pour signaler l'impossibilite d'executer une 
+ * operation sur une file lorsque celle-ci est vide.
+ * @author Melanie Lord
+ * @version H24
+ */
+public class FileVideException extends Exception {
+
+}
